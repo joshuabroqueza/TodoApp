93217f1fd3567af4c0a40be4aff379bc661b90c8
Joshua Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2020-04-18 11:13:42 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.scss";
+import TodoApp from './TodoApp';
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      <TodoApp/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
